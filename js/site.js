f7c32d43cad4aef3a99a8295b41b06cc5df6ae24
Aptenz <59879368+Aptenz@users.noreply.github.com>
2022-08-21 18:58:16 -0600

    basic js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,28 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+// get the values from the Page
+// starts or controller function
+
+function getValues(){
+    // get values
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start Value: " + startValue);
+    
+    // call generateNumbers
+
+    //we call displayNumbers
+
+}
+
+// generateNumbers from startvalue to the endValue
+// logic function(s)
+function generateNumbers(){
+
+}
+
+
+// display the numbers and mark even numbers
+// display or view functions
+function displayNumbers(){
+
 }
